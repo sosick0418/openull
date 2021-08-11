65ab21e17881acc16b4d9d8512b7a9a49d9fc9f9
선종권 <jonggwon@seonjong-gwon-ui-MacBookPro.local>
2021-08-11 22:32:12 +0900

    api 작성

--- CodingTest/src/api/index.ts
+++ CodingTest/src/api/index.ts
@@ -1,1 +1,39 @@
-export {};
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+function createInstance(): AxiosInstance {
+  const instance = axios.create({
+    baseURL: 'https://mock-api.ssomee.com/',
+  });
+  return instance;
+}
+
+const instance = createInstance();
+
+export const getCategoryList = async (): Promise<AxiosResponse<any>> => {
+  return await instance.get('categories');
+};
+
+export const getAllItemsList = async (
+  page: number,
+  order: string,
+): Promise<AxiosResponse<any>> => {
+  return await instance.get(`products/all/${page}?order=${order}`);
+};
+
+export const getCatetoryItemsList = async (
+  categoryId: string,
+  page: number,
+  order: string,
+): Promise<AxiosResponse<any>> => {
+  return await instance.get(`products/${categoryId}/${page}?order=${order}`);
+};
+
+export const getItemDetail = async (
+  prefix: string,
+): Promise<AxiosResponse<any>> => {
+  return await instance.get(`products/${prefix}}`);
+};
+
+export const postBuy = async (prefix: string): Promise<AxiosResponse<any>> => {
+  return await instance.post(`products/${prefix}}`);
+};
--- CodingTest/types.ts
+++ CodingTest/types.ts
@@ -1,3 +1,1 @@
-export type RootStackParamList = {
-  Root: undefined;
-};
+export {};
